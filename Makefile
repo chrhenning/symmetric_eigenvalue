4ca58623826a43d605099dad5be4c0b2aa11a94c
Christian Henning <henning@finf.uni-hannover.de>
2016-04-05 17:07:28 -0400

    implemented argument parsing

--- Makefile
+++ Makefile
@@ -12,14 +12,14 @@ COMPILER_FLAGS_CC = $(COMPILER_FLAGS) -fopenmp
 COMPILER_FLAGS_CC_MPI = $(COMPILER_FLAGS) -openmp
 
 #OBJ_NAME specifies the name of our exectuable 
-OBJ_NAME = cuppen
+OBJ_NAME = cuppens
 
 MKL_MIC_ENABLE=1
 MKL =    ${MKLROOT}/lib/intel64/libmkl_scalapack_ilp64.a -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_core.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_blacs_intelmpi_ilp64.a -Wl,--end-group -lpthread -lm
 FLAGS=  -DMKL_ILP64 -qopenmp -I${MKLROOT}/include -O3
 
 #This is the target that compiles our executable 
-all : cuppen  
+all : cuppen 
 	
 cuppen: $(OBJs)
 	$(CC_MPI) $(FLAGS) $(OBJS) -o $(OBJ_NAME) $(MKL)
--- src/main.c
+++ src/main.c
@@ -1,7 +1,114 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+void showHelp();
+
+int main (int argc, char **argv)
 {
-    printf("Hello World!\n");
+    // ///////////////////////////
+    // parse command line arguments
+    // ///////////////////////////
+
+    // no parameters are given, thus print usage hints and close programm
+    if (argc == 1) {
+        showHelp();
+        return 0;
+    }
+
+    char* inputfile = NULL;
+    char* outputfile = NULL;
+
+    /*
+     * Scheme to used as specified by option -s
+     */
+    int usedScheme = 1;
+    int n = 100; // size of predefined matrix
+
+    int c;
+
+    opterr = 0;
+
+    while ((c = getopt (argc, argv, "hi:n:s:")) != -1)
+        switch (c)
+        {
+        case 'h':
+            showHelp();
+            return 0;
+        case 'i':
+            inputfile = optarg;
+            break;
+        case 's':
+            usedScheme = atoi(optarg); // keep in mind, that atoi returns 0,  if string is not and integer
+            if (usedScheme < 1 || usedScheme > 2) {
+                fprintf (stderr, "Invalid argument for option -s. See help.\n");
+                return 1;
+            }
+            break;
+        case 'n':
+            n = atoi(optarg);
+            if (n < 1) {
+                fprintf (stderr, "Invalid argument for option -n. See help.\n");
+                return 1;
+            }
+            break;
+        case '?':
+            if (isprint (optopt))
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf (stderr,
+                         "Unknown option character `\\x%x'.\n", optopt);
+            return 1;
+        default:
+            abort ();
+        }
+
+    // if there are more than one positional argument
+    if (argc - optind > 1) {
+        fprintf (stderr, "Invalid number of positional arguments. See help.\n");
+        return 1;
+    }
+
+    outputfile = argv[optind];
+
+    // print settings
+    if (inputfile != NULL)
+        printf("Input file: %s\n", inputfile);
+    else
+        printf("Use a matrix of scheme %d with dimension %d\n", usedScheme, n);
+
+    if (outputfile != NULL)
+        printf("Output file: %s\n", outputfile);
+
     return 0;
+}
+
+/**
+ * @brief showHelp Show usage details to the user
+ */
+void showHelp() {
+    printf("\n");
+    printf("USAGE cuppens [options] [outputfile]\n");
+    printf("\n");
+    printf("The program can compute all the eigenvalues of a matrix on a parallel machine\n");
+    printf("by using cuppens algorithm\n");
+    printf("The results can be written into an outputfile, if specified.\n");
+    printf("\n");
+    printf("OPTIONS\n");
+    printf(" -h\n");
+    printf("    Show help.\n");
+    printf(" -i FILENAME\n");
+    printf("    The name of a file which contains a tridiagonal matrix in mtx format.\n");
+    printf("    The eigenvalues of this matrix will then be computed.\n");
+    printf(" -s NUM\n");
+    printf("    If you want to compute the eigenvalues of a predefined matrix, you may.\n");
+    printf("    use this option to define the scheme of the matrix.\n");
+    printf("    1 - eigenvalue i has the form: 4 - 2*cos((PI*i)/(n+1)) \n");
+    printf("    2 - Matrix will have the tridiagonal form [-1,d_i,-1] where the diagonal\n");
+    printf("        elements will be evenly spaced in the interval [1,100] \n");
+    printf("    If option i is used, then this option will be ignored.\n");
+    printf(" -n NUM\n");
+    printf("    Specify the dimension of the matrix chosen with option -s.\n");
+    printf("\n");
 }
