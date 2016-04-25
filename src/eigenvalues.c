c79894026bdb2809242b8d8fef1a0bd45bef5df7
Qi Wang <wang2047@mc03.cs.purdue.edu>
2016-04-25 12:16:08 -0400

    Modified D after Givens rotation

--- src/eigenvalues.c
+++ src/eigenvalues.c
@@ -82,7 +82,7 @@ void computeEigenvalues(EVRepNode* node, MPIHandle mpiHandle) {
    * element that corresponds to the smaller diagonal element to be zero*/
 
     int a, b;
-    double r;
+    double c, s ,r, tmpi, tmpj;
     int nextNonZero;
     for (i = 0; i < n - 1; i++){
         if (G[SD[i].i] != -2) { // for those elements correspond to non-zero z
@@ -93,124 +93,133 @@ void computeEigenvalues(EVRepNode* node, MPIHandle mpiHandle) {
             if (nextNonZero >= n) continue;
 
             if (fabs(SD[nextNonZero].e - SD[i].e) < eps) {
-                a = SD[i].i;
-                b = SD[nextNonZero].i;
-                r = sqrt(z[a] * z[a] + z[b] * z[b]);
-                C[node->numGR] = z[b] / r;
+              a = SD[i].i;
+              b = SD[nextNonZero].i;
+              r = sqrt(z[a] * z[a] + z[b] * z[b]);
+              c = z[b] / r;
+              s = z[a] / r;
+              C[node->numGR] = c;
 
-                G[SD[i].i] = SD[nextNonZero].i;
-                z[SD[nextNonZero].i] = sqrt(z[SD[nextNonZero].i] * z[SD[nextNonZero].i] + z[SD[i].i] * z[SD[i].i]);
-                z[SD[i].i] = 0;
-                P[node->numGR] = SD[i].i;
 
-                node->numGR++;
+              G[SD[i].i] = SD[nextNonZero].i;
+              z[SD[nextNonZero].i] = sqrt(z[SD[nextNonZero].i] * z[SD[nextNonZero].i] + z[SD[i].i] * z[SD[i].i]);
+              z[SD[i].i] = 0;
+              P[node->numGR] = SD[i].i;
+
+              tmpi = c * c * SD[i].e + s * s * SD[nextNonZero].e;
+              tmpj = s * s * SD[i].e + c * c * SD[nextNonZero].e;
+              SD[i].e = tmpi;
+              SD[nextNonZero].e = tmpj;
+              D[a] = tmpi;
+              D[b] = tmpj;
+              node->numGR++;
             }
             else
-                G[SD[i].i] = -1;
+              G[SD[i].i] = -1;
         }
     }
 
     /* Note, if roh > 0, then the last eigenvalue is behind the last d_i
-   * If roh < 0, then the first eigenvalue is before the first d_i */
+     * If roh < 0, then the first eigenvalue is before the first d_i */
 
     // use norm of z as an approximation to find the first resp. last eigenvalue
     double normZ = cblas_dnrm2(n, z, 1);
 
     /******************
-   * Simple Bisection algorithm
-   * ****************/
+     * Simple Bisection algorithm
+     * ****************/
     long maxIter = 10000;
     /*
-     N ← 1
-     While N ≤ NMAX # limit iterations to prevent infinite loop
-     c ← (a + b)/2 # new midpoint
-     If f(c) = 0 or (b – a)/2 < TOL then # solution found
-     Output(c)
-     Stop
-     EndIf
-     N ← N + 1 # increment step counter
-     If sign(f(c)) = sign(f(a)) then a ← c else b ← c # new interval
-     EndWhile
-     */
+       N ← 1
+       While N ≤ NMAX # limit iterations to prevent infinite loop
+       c ← (a + b)/2 # new midpoint
+       If f(c) = 0 or (b – a)/2 < TOL then # solution found
+       Output(c)
+       Stop
+       EndIf
+       N ← N + 1 # increment step counter
+       If sign(f(c)) = sign(f(a)) then a ← c else b ← c # new interval
+       EndWhile
+       */
     //#pragma omp parallel for default(shared) private(i) schedule(static)
     for (i = 0; i < n; ++i) { // for each eigenvalue
-        double lambda = 0;
-        double a, b; // interval boundaries
-        double fa, flambda, fb; // function values
+      double lambda = 0;
+      double a, b; // interval boundaries
+      double fa, flambda, fb; // function values
 
-        int ind = SD[i].i;
-        int prevNonZeroIdx;
-        double di = SD[i].e;
+      int ind = SD[i].i;
+      int prevNonZeroIdx;
+      double di = SD[i].e;
 
-        if (G[ind] != -1) {
-            L[ind] = di;
-        } else {
-            // set initial interval
-            if (roh < 0) {
-                if (i == 0) {
-                    a = di - normZ;
-                    int j = 0;
-                    while(secularEquation(a, roh, z, D, n, G) < 0) {
-                        a -= normZ;
-                        assert(++j < 100);
-                    }
-                } else {
-                    prevNonZeroIdx = i - 1;
-                    while(G[SD[prevNonZeroIdx].i] != -1) // TODO: Take the first element is zero into consideration
-                        prevNonZeroIdx = prevNonZeroIdx - 1;
-                    a = SD[prevNonZeroIdx].e;
-                }
-                b = di;
-            } else {
-                a = di;
-                if (i == n-1) {
-                    b = di + normZ;
-                    int j = 0;
-                    while(secularEquation(b, roh, z, D, n, G) < 0) {
-                        b += normZ;
-                        assert(++j < 100);
-                    }
-                } else {
-                    prevNonZeroIdx = i + 1;
-                    while(G[SD[prevNonZeroIdx].i] != -1) // TODO: Take the last element is zero into consideration
-                        prevNonZeroIdx = prevNonZeroIdx + 1;
-                    b = SD[prevNonZeroIdx].e;
-                }
-            }
-
+      if (G[ind] != -1) {
+        L[ind] = di;
+      } else {
+        // set initial interval
+        if (roh < 0) {
+          if (i == 0) {
+            a = di - normZ;
             int j = 0;
-            while (++j < maxIter) {
-
-                // new lambda
-                lambda = (a+b) / 2;
-                // compute current function values
-                fa = secularEquation(a, roh, z, D, n, G);
-                flambda = secularEquation(lambda, roh, z, D, n, G);
-                //fb = secularEquation(b, roh, z, D, n, G);
-
-                // if a function value is inf, then it has probably not the right sign
-                // initial function values are in +/- infinity, depending on the gradiend of the secular equation
-                if (fa == INFINITY || fa == -INFINITY)
-                    fa = (roh > 0 ? -INFINITY : INFINITY);
-
-                //if (fb == INFINITY || fb == -INFINITY)
-                //   fb = (roh > 0 ? INFINITY : -INFINITY);
-
-                //if (j==10)
-                //    printf("interval: %g, %g, %g, %g, %g, %g\n", fa, flambda, fb, a, lambda, b);
-
-                if (flambda == 0 || (b-a)/2 < eps)
-                    break;
-
-                // if sign(a) == sign(lambda)
-                if ((fa >= 0 && flambda >= 0) || (fa < 0 && flambda < 0))
-                    a = lambda;
-                else
-                    b = lambda;
+            while(secularEquation(a, roh, z, D, n, G) < 0) {
+              a -= normZ;
+              assert(++j < 100);
             }
-            L[ind] = lambda;
-            //printf("f(%g) = %g\n", lambda, secularEquation(lambda, roh, z, D, n, G));
+          } else {
+            prevNonZeroIdx = i - 1;
+            while(G[SD[prevNonZeroIdx].i] != -1) // TODO: Take the first element is zero into consideration
+              prevNonZeroIdx = prevNonZeroIdx - 1;
+            a = SD[prevNonZeroIdx].e;
+          }
+          b = di;
+        } else {
+          a = di;
+          if (i == n-1) {
+            b = di + normZ;
+            int j = 0;
+            while(secularEquation(b, roh, z, D, n, G) < 0) {
+              b += normZ;
+              assert(++j < 100);
+            }
+          } else {
+            prevNonZeroIdx = i + 1;
+            while(G[SD[prevNonZeroIdx].i] != -1) // TODO: Take the last element is zero into consideration
+              prevNonZeroIdx = prevNonZeroIdx + 1;
+            b = SD[prevNonZeroIdx].e;
+          }
         }
+
+        int j = 0;
+        while (++j < maxIter) {
+
+          // new lambda
+          lambda = (a+b) / 2;
+          // compute current function values
+          fa = secularEquation(a, roh, z, D, n, G);
+          flambda = secularEquation(lambda, roh, z, D, n, G);
+          //fb = secularEquation(b, roh, z, D, n, G);
+
+          // if a function value is inf, then it has probably not the right sign
+          // initial function values are in +/- infinity, depending on the gradiend of the secular equation
+          if (fa == INFINITY || fa == -INFINITY)
+            fa = (roh > 0 ? -INFINITY : INFINITY);
+
+          //if (fb == INFINITY || fb == -INFINITY)
+          //   fb = (roh > 0 ? INFINITY : -INFINITY);
+
+          //if (j==10)
+          //    printf("interval: %g, %g, %g, %g, %g, %g\n", fa, flambda, fb, a, lambda, b);
+
+          if (flambda == 0 || (b-a)/2 < eps)
+            break;
+
+          // if sign(a) == sign(lambda)
+          if ((fa >= 0 && flambda >= 0) || (fa < 0 && flambda < 0))
+            a = lambda;
+          else
+            b = lambda;
+        }
+        L[ind] = lambda;
+        //printf("f(%g) = %g\n", lambda, secularEquation(lambda, roh, z, D, n, G));
+      }
     }
 
 
@@ -222,90 +231,90 @@ void computeEigenvalues(EVRepNode* node, MPIHandle mpiHandle) {
 }
 
 double* computeNormalizationFactors(double* D, double* z, double* L, int *G, int n) {
-    double *N = malloc(n * sizeof(double));
+  double *N = malloc(n * sizeof(double));
 
-    // TODO: what we probably should do here, is setting all normalization vectors to one, and
-    // we call getEigenVector to get each single eigenvector and compute the norm of it
+  // TODO: what we probably should do here, is setting all normalization vectors to one, and
+  // we call getEigenVector to get each single eigenvector and compute the norm of it
 
-    int i, j;
-    double tmp;
-    //#pragma omp parallel for default(shared) private(i,j,tmp) schedule(static)
-    for (i = 0; i < n; ++i) {
-        if (G[i] != -1) {
-            N[i] = 1;
-        } else {
-            N[i] = 0;
-            for (j = 0; j < n; ++j) {
-                if (G[j] == -1) {
-                    tmp = L[i] - D[j];
-                    //printf("i =  %d, j = %d  tmp is %lg\n", i, j, tmp);
+  int i, j;
+  double tmp;
+  //#pragma omp parallel for default(shared) private(i,j,tmp) schedule(static)
+  for (i = 0; i < n; ++i) {
+    if (G[i] != -1) {
+      N[i] = 1;
+    } else {
+      N[i] = 0;
+      for (j = 0; j < n; ++j) {
+        if (G[j] == -1) {
+          tmp = L[i] - D[j];
+          //printf("i =  %d, j = %d  tmp is %lg\n", i, j, tmp);
 
-                    N[i] += z[j]*z[j] / (tmp*tmp);
-                }
-            }
-            //printf("%d N[i] is %lg\n", i, N[i]);
-            N[i] = sqrt(N[i]);
+          N[i] += z[j]*z[j] / (tmp*tmp);
         }
+      }
+      //printf("%d N[i] is %lg\n", i, N[i]);
+      N[i] = sqrt(N[i]);
     }
+  }
 
-    return N;
+  return N;
 }
 
 void getEigenVector(EVRepNode *node, double* ev, int i) {
-    double* D = node->D;
-    double* z = node->z;
-    double* L = node->L;
-    double* N = node->N;
-    double* C = node->C;
-    int* G = node->G;
-    int* P = node->P;
-    double roh = node->beta * node->theta;
-    int n = node->n;
-    int numGR = node->numGR;
+  double* D = node->D;
+  double* z = node->z;
+  double* L = node->L;
+  double* N = node->N;
+  double* C = node->C;
+  int* G = node->G;
+  int* P = node->P;
+  double roh = node->beta * node->theta;
+  int n = node->n;
+  int numGR = node->numGR;
 
 
-    // TODO compute i-th eigenvector and store in ev
-    int j;
-    if(G[i] != -1) {
-        for (j = 0; j < n; j++) {
-            if (j == i){
-                ev[j] = 1;
-            } else {
-                ev[j] = 0;
-            }
-        }
-    } else {
-        for (j = 0; j < n; j ++)
-            if (G[j] < -1) {
-                ev[j] = 0;
-            } else {
-                ev[j] = z[j] / ((D[j] - L[i]) * N[i]);
-            }
-
+  // TODO compute i-th eigenvector and store in ev
+  int j;
+  if(G[i] != -1) {
+    for (j = 0; j < n; j++) {
+      if (j == i){
+        ev[j] = 1;
+      } else {
+        ev[j] = 0;
+      }
     }
+  } else {
+    for (j = 0; j < n; j ++)
+      if (G[j] < -1) {
+        ev[j] = 0;
+      } else {
+        ev[j] = z[j] / ((D[j] - L[i]) * N[i]);
+      }
+
+  }
 
 
-    /* recover the original rank-one update
+  /* recover the original rank-one update
    * apply the inverse of Givens rotation from outside to inside
    * for example, if the Givens rotation on the original problem is
    * G3 * G2 * G1 * (D + zz') G1' * G2' * G3'
    * The order here should be G3^-1, G2^-1 nad G1^-1 */
 
-    //#pragma omp parallel for default(shared) private(j) schedule(static)
-    for (j = numGR - 1; j >= 0 ; j--) {
-        int a, b;
-        double s, c;
-        double tmpi, tmpj;
+  //#pragma omp parallel for default(shared) private(j) schedule(static)
+  for (j = numGR - 1; j >= 0 ; j--) {
+    int a, b;
+    double s, c;
+    double tmpi, tmpj;
 
-        a = P[j];
-        b = G[a];
-        c = C[j];
-        s = sqrt(1 - c * c); // TODO: probably it's better to store s as well, since the product c*c halves the precision (e^-10 * e^-10 = e^-20)
+    a = P[j];
+    b = G[a];
+    c = C[j];
+    s = sqrt(1 - c * c); // TODO: probably it's better to store s as well, since the product c*c halves the precision (e^-10 * e^-10 = e^-20)
 
-        tmpi = c * ev[a] + s * ev[b];
-        tmpj = -s * ev[a] + c * ev[b];
-        ev[a] = tmpi;
-        ev[b] = tmpj;
-    }
+    tmpi = c * ev[a] + s * ev[b];
+    tmpj = -s * ev[a] + c * ev[b];
+    ev[a] = tmpi;
+    ev[b] = tmpj;
+  }
 }
 
